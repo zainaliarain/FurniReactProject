15b20932a7bba2a692814c92be214d547f51f389
Zain786101 <zaynaliarain@gmail.com>
2024-04-03 02:57:55 +0500

    Recent work start

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,27 +1,62 @@
 import React from "react";
 import recentImg1 from "../images/post-1.jpg";
+import recentImg2 from "../images/post-2.jpg";
+import recentImg3 from "../images/post-3.jpg";
+import "./Blog.css";
 // import recentImg2 from "../images/post-1.jpg";
 // import recentImg3 from "../images/post-1.jpg";
 function Blog() {
+  const Blog = [
+    {
+      icon: recentImg1,
+      title: "First Time Home Owner Ideas",
+      author: "Kristin Watson",
+      date: "Dec 19, 2021",
+    },
+    {
+      icon: recentImg2,
+      title: "How To Keep Your Furniture Clean",
+      author: "Robert Fox",
+      date: "Dec 15, 2021",
+    },
+    {
+      icon: recentImg3,
+      title: "Small Space Furniture Apartment Ideas",
+      author: "Kristin Watson",
+      date: "Dec 12, 2021",
+    },
+  ];
   return (
     <div>
-      <div className="container">
-        <div className="row-mt-5">
-          <div className="col-6">
+      <div className="container" style={{ marginTop: "10%" }}>
+        <div className="row mb-5 mt-5 ">
+          <div className="col-6 mb-5">
             <h2>Recent Blog</h2>
           </div>
-          <div className="col-6">
-            <a href="#">View All Posts</a>
+          <div className="col-6 card-header">
+            <a href="/">View All Posts</a>
           </div>
-          <div className="row-mt-5">
-            <div className="col-4">
-              <div class="card">
-                <div class="card-body">
-                  <img alt="Post for Recent" src={recentImg1} />
-                  <a href="">First Time Home Owner Ideas</a>
+          <div className="row ">
+            {Blog.map((Blog, index) => (
+              <div className="col-4" key={index}>
+                <div class="card" style={{ border: "none" }}>
+                  <div class="card-body">
+                    <a href="/link/to/somewhere">
+                      <img alt="Post for Recent" src={Blog.icon} />
+                    </a>
+                    <a href="/link/to/#" style={{ marginTop: "20px" }}>
+                      {Blog.title}
+                    </a>
+                    <div className="cardfooter">
+                      <span>
+                        by <a href="/link/to/somewhere">{Blog.author}</a>on
+                        <a href="/link/to/somewhere">{Blog.date}</a>
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
